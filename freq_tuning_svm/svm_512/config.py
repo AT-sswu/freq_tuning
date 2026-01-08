5490244b4b8fff9a0b4514c5fc00afbfb1599cdd
kseohyuz <seohyun518612@naver.com>
2026-01-08 21:37:09 +0900

    Feat: svm_512 train

---
+++ freq_tuning_svm/svm_512/config.py
@@ -0,0 +1,25 @@
+# 경로 설정
+DATA_DIR = "/Users/seohyeon/AT_freq_tuning/data_v3"
+OUTPUT_DIR = "/Users/seohyeon/AT_freq_tuning/freq_tuning_svm/svm_512/preprocessed_data"
+MODEL_DIR = "/Users/seohyeon/AT_freq_tuning/freq_tuning_svm/svm_512/model_results"
+
+# 공진 주파수 (고정)
+RESONANCE_FREQS = [40, 50, 60]
+
+# 데이터 증강 설정
+WINDOW_SIZE = 512
+STRIDE = 256
+AUGMENTATION_SNR_DB = [-10, -5, 5, 10]
+ENABLE_AUGMENTATION = True
+
+# 데이터 분할 설정
+TEST_SIZE = 0.2
+RANDOM_STATE = 42
+
+# SVM 하이퍼파라미터
+SVM_CONFIG = {
+    'kernel': 'rbf',
+    'C': 10,
+    'gamma': 'scale',
+    'random_state': 42
+}
